b1a19db4a9226d215eee750cd35467896327bdc2
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-11 19:54:44 -0800

    testing datatypes

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+print("Check code for how I did this and a couple of additional comments!")
+print("\n")
+
 print("Hello World!")
-var1 = 123;
+print("\n")
+
+var1 = 123
 var2 = "Hello World!"
 print(var2)
 print(var1)
+print("\n")
+
 print("var1 : ", type(var1))
 print("var2 : ", type(var2))
+print("\n")
+
+x = 1
+y = 2.8
+# z is a complex datatype
+z = 1j
+print("\n")
+
+# datatype conversion examples
+print("'x' datatype before conversion: ", type(x));
+a = float(x)
+print("'x' datatype after conversion: ", type(a));
+print("'y' datatype before conversion: ", type(y));
+b = int(y)
+print("'y' datatype after conversion: ", type(b));
+# complex(x) is a conversion from int to complex datatype
+print("('x' datatype after conversion) 'a' datatype before conversion: ", type(a));
+c = complex(x)
+print("'a' datatype after conversion: ", type(c));
