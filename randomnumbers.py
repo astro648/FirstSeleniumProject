67bbbc421bbe73eb8fdc0a7d5eba98606b2b4595
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-11 23:58:13 -0800

    working on 10 random even and odd numbers
    this is not working as of right now

--- randomnumbers.py
+++ randomnumbers.py
@@ -1,7 +1,23 @@
 import random
 
 print("Generate 10 Random Numbers from 1-100")
-print("\n")
-
 for i in range(10):
     print(random.randint(1, 100))
+print("\n")
+
+print("Generate 10 Random Even Numbers")
+counteven = 1
+for counteven in range(10):
+    a = random.randint(1, 100)
+    if a == int(a):
+        print(a)
+        counteven = counteven + 1
+print("\n")
+
+print("Generate 10 Random Odd Numbers")
+countodd = 1
+for countodd in range(10):
+    a = random.randint(1, 100)
+    if a != int(a):
+        print(a)
+        countodd = countodd + 1
