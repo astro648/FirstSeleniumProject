691d1bd90d39d333b541761867731180c232cdb6
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-17 14:21:47 -0800

    title

--- sysmonitor.py
+++ sysmonitor.py
@@ -3,3 +3,8 @@ import time
 from subprocess import call
 from prettytable import PrettyTable
 
+while True:
+    call('clear')
+    print("==============================Process Monitor\
+        ======================================")
+    
