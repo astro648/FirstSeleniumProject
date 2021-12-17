813dd4a3b4a57fa743360243be797cda9b4258db
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-17 14:25:39 -0800

    last 10 processes and delay

--- sysmonitor.py
+++ sysmonitor.py
@@ -35,3 +35,28 @@ while True:
         vm.percent
     ])
     print(memory_table)
+
+    # Last 10 processes
+    print("----Last 10 Processes----")
+    process_table = PrettyTable(['PID', 'PNAME', 'STATUS',
+                                 'CPU', 'NUM THREADS'])
+
+    for process in psutil.pids()[-10:]:
+        # While fetching processes, some of the subprocesses may exit
+        # That is why we put it in a try-except
+        try:
+            p = psutil.Process(process)
+            process_table.add_row([
+                str(process),
+                p.name(),
+                p.status(),
+                str(p.cpu_percent()) + "%",
+                p.num_threads()
+            ])
+
+        except Exception as e:
+            pass
+    print(process_table)
+
+    # 0.5s delay
+    time.sleep(0.5)
