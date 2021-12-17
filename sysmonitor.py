b4b8058ba43f41349d3b530459624a2233c82209
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-17 14:23:28 -0800

    network and memory info

--- sysmonitor.py
+++ sysmonitor.py
@@ -13,4 +13,25 @@ while True:
     battery = psutil.sensors_battery().percent
     print("----Battery Available: %d " % (battery,) + "%")
 
-    #
+    # Network Info
+    print("----Network Info----")
+    table = PrettyTable(['Network', 'Status', 'Speed'])
+    for key in psutil.net_if_stats().keys():
+        name = key
+        up = "Up" if psutil.net_if_stats()[key].isup else "Down"
+        speed = psutil.net_if_stats()[key].speed
+        table.add_row([name, up, speed])
+    print(table)
+
+    # Memory Info
+    print("----Memory----")
+    memory_table = PrettyTable(["Total", "Used",
+                                "Available", "Percentage"])
+    vm = psutil.virtual_memory()
+    memory_table.add_row([
+        vm.total,
+        vm.used,
+        vm.available,
+        vm.percent
+    ])
+    print(memory_table)
