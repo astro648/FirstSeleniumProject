b413041cbffddeea190a3b7714c738c4c413dcc3
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-18 19:58:33 -0800

    Update stringslicing.py

--- stringslicing.py
+++ stringslicing.py
@@ -4,4 +4,5 @@ session = "Selenium Web Driver"
 # array always starts at 0
 # so instead of counting 1,2,3,4,5,6
 # you would count 0,1,2,3,4,5
-print(session[0:8])
+print(session[:8])
+# if you want to start from 0, instead of writing [0:y] just write [:y]
