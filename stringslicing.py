25dfa90332e019a1ee1d97414d9e03767998121b
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-18 19:57:31 -0800

    arrays

---
+++ stringslicing.py
@@ -0,0 +1,7 @@
+session = "Selenium Web Driver"
+# [x:y] <-- Array
+# x is starting value, y is ending value
+# array always starts at 0
+# so instead of counting 1,2,3,4,5,6
+# you would count 0,1,2,3,4,5
+print(session[0:8])
