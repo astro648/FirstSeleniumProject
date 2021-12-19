62e5dbf022bbabd2b6fcc818948070947d3d6ee9
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-18 20:00:11 -0800

    string start from end

--- stringslicing.py
+++ stringslicing.py
@@ -6,3 +6,6 @@ session = "Selenium Web Driver"
 # you would count 0,1,2,3,4,5
 print(session[:8])
 # if you want to start from 0, instead of writing [0:y] just write [:y]
+print(session[0:])
+# in this situation lets call z the end value
+# if you want to start from the end, instead of writing [x:z] you just write [x:]
