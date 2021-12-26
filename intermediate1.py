3b1df7f9aeb2ffd3f0bd99cf8e51cef53a549de7
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-25 22:20:08 -0800

    Update intermediate1.py

--- intermediate1.py
+++ intermediate1.py
@@ -14,3 +14,32 @@ elif x == 0:
     print(x," = 0")
 else:
     print(x," is a negative number")
+a = 330
+b = 330
+print("A") if a > b else print(a,"=",b) if a == b else print("B")
+a = 2
+b = 10
+print(a) if a < b else print(b)
+if b > a:
+  pass
+#while b > a:
+#    if b == 5:
+#        continue
+#    print(b)
+#    b = b-1
+#i = 0
+#while i < 6:
+#  i += 1
+#  if i == 3:
+#    continue
+#  print(i)
+for z in mySet: # this prints mySet
+    print(z)
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+dict1 = {1: "Bitcoin", 2: "Ethereum"}
+for key, value in dict1.items():
+    print(f"Key {key} has value {value}")
