c9efe522813788f42b561daa9c2cfc1ea64fb555
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-25 19:21:51 -0800

    replacing and placing

--- intermediateconcepts.py
+++ intermediateconcepts.py
@@ -5,3 +5,6 @@ print(myList[1]) # prints second value
 print(myList[-1]) # prints last value
 print(myList[-2]) # prints second to last value
 print(myList[2:5]) # prints values 3-6, similar to string slicing
+myList[2] = "lemon" # replaces 3rd value "cherry" in myList with "lemon"
+myList.insert(2, "watermelon") # places "watermelon" in 3rd value between "banana" and "orange"
+print(myList) # prints the new list
