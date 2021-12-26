5793cd81ed88b7349158ffe0de991c13251af8d8
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-25 19:17:48 -0800

    print specified values

---
+++ intermediateconcepts.py
@@ -0,0 +1,7 @@
+print("Go to the code to see what's going on here!")
+myList = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(myList) # prints entire list
+print(myList[1]) # prints second value
+print(myList[-1]) # prints last value
+print(myList[-2]) # prints second to last value
+print(myList[2:5]) # prints values 3-6, similar to string slicing
