0d7a1b1f2ecee11ee42bb67139e89c44c2e3efa4
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-25 20:22:07 -0800

    asdf

---
+++ intermediate1.py
@@ -0,0 +1,16 @@
+mySet = {"webbtelescope", 123, True} # Sets are made
+# similarly to lists but they can be any types of values and are unordered
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 2021,
+  "color": ["Twister Orange", "Yellow Peel", "Lucid Red Pearl", "Velocity Blue", "Ford Performance Blue", "Absolute Black", "Antimatter Blue", "Iconic Silver Metallic", "Carbonized Gray Metallic", "Fighter Jet Gray", "Race Red", "Oxford White"]
+}
+print(thisdict)
+x = 1324865823465
+if x > 0:
+    print(x, " is a positive number")
+elif x == 0:
+    print(x," = 0")
+else:
+    print(x," is a negative number")
