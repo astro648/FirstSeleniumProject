0b675c0c37b43a40c1283d8e43b4ff9bc65aa2b0
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-26 17:54:26 -0800

    Update functions.py

---
+++ functions.py
@@ -0,0 +1,17 @@
+def display(text): # define a function called display() with parameter of text
+    print(text)
+display("Hello World") # parameter text in this case is "Hello World"
+
+def family(*kids): # function family with parameter kids
+    # the asterisk makes it so that you can pass any amount of things for the parameter "kids"
+    print("Youngest child is: "+kids[2]) # in this example we don't know how many values there are
+family("Joe","John","Jane") # above, kids[2] makes it so that Jane, the 3rd value, is shown
+
+def my_function(**kid):
+  print("Her last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+def my_family(youngest="Jane"):
+    print("The youngest child is: ",youngest) # using predefined parameters
+my_family()
