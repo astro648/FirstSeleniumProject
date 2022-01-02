c929e42e08f765ec0fc11c2e30a44f66536a8c2d
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-01 19:37:42 -0800

    2 write 1 more read

--- filehandling.py
+++ filehandling.py
@@ -1,2 +1,12 @@
 file = open('sample.txt','r')
 print(file.read(5)) # prints first 5 characters in selected file
+file.close() # closes file for this instance
+filewrite = open('sample.txt','w')
+filewrite.write("Happy new year!") # overwrites existing text with "Happy new year!" in the file
+filewrite.write(" It's 2022 now") # appends It's 2022 now to Happy New Year!
+filewrite.close()
+file2 = open('sample.txt','r')
+print(file2.read()) #prints the entire file
+file2.close()
+filewrite2 = open('sample.txt','w')
+filewrite2.write("Kali Linux can be used for hacking") # completely overwrites the above text with this
