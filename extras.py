60a4ac092e87334564ff79cc75f30759589d6881
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-07 17:43:43 -0800

    moved some code to mymodule.py

--- extras.py
+++ extras.py
@@ -1,14 +1,3 @@
-# When a single variable is declared in the main body of the program,
-# it will work as a global variable for functions
-x = 300
-
-
-def myfunc():
-    global x # this changes the global variable x
-    x = 200 # to 200
-    print(x)
-
-
 myfunc()
 
 print(x)  # it is still 200
---
+++ mymodule.py
@@ -0,0 +1,9 @@
+# When a single variable is declared in the main body of the program,
+# it will work as a global variable for functions
+x = 300
+
+
+def myfunc():
+    global x # this changes the global variable x
+    x = 200 # to 200
+    print(x)
