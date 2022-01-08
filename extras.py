bdbac9419b9499fc7ba63d4ffc0fe05dba1d0bfb
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-07 17:41:51 -0800

    global variables and notes

--- extras.py
+++ extras.py
@@ -1,14 +1,14 @@
+# When a single variable is declared in the main body of the program,
+# it will work as a global variable for functions
 x = 300
 
+
 def myfunc():
-  print(x)
+    global x # this changes the global variable x
+    x = 200 # to 200
+    print(x)
+
 
 myfunc()
 
-print(x)
-# The function was called to print(x) and after that print(x) was directly called
-# so it printed twice
-
-# When a single variable is declared in the main body of the program,
-# it will work as a global variable for functions
-
+print(x)  # it is still 200
