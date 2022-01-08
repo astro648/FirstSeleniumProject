a04703aa76c855896e8fae1e478ad85a1ed17d0f
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-07 17:57:11 -0800

    built-in time functions

--- extras.py
+++ extras.py
@@ -1,5 +1,5 @@
 import mymodule as mm  # mm replaces mymodule when needed
-
+import datetime as dt  # dt replaces datetime
 from mymodule import person1  # imports library person1
 
 mm.myfunc()  # instead of writing mymodule.myfunc() we write mm.myfunc()
@@ -7,3 +7,10 @@ x = mm.x  # uses the x value from mm
 print(x)  # it is still 200, referencing changing the global variable
 
 print(person1["name"])  # prints value "name" from library person1
+
+x = dt.datetime.now()
+print(x)  # prints current date and time
+print(x.year)  # only prints current year
+print(x.strftime("%A"))  # prints current day's name (e.g. Friday)
+# if %a (lowercase) is used, only Fri from above example will print
+
