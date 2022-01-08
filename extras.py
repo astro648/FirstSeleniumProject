4fc6f5033681b20c7b0499fd0cf981a74c16730d
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-07 17:50:23 -0800

    uses library from other file

--- extras.py
+++ extras.py
@@ -1,5 +1,9 @@
 import mymodule as mm  # mm replaces mymodule when needed
 
+from mymodule import person1  # imports library person1
+
 mm.myfunc()  # instead of writing mymodule.myfunc() we write mm.myfunc()
-x = mm.x     # uses the x value from mm
-print(x)     # it is still 200, referencing changing the global variable
+x = mm.x  # uses the x value from mm
+print(x)  # it is still 200, referencing changing the global variable
+
+print(person1["name"])  # prints value "name" from library person1
