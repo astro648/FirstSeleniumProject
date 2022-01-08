e8c8f33153fc62822a5ef3572332332fc72972d1
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-07 18:23:26 -0800

    more regex and formatting

--- regex.py
+++ regex.py
@@ -1,9 +1,20 @@
-import re # imports regex functionality
+import re  # imports regex functionality
+
 # RegEx means Regular Expression
-txt = "The rain in Taiwan" # text to be search
-x = re.search("^The.*USA$", txt) # this line checks whether txt starts with "The"
+txt = "The rain in Taiwan"  # text to be search
+x = re.search("^The.*USA$", txt)  # this line checks whether txt starts with "The"
 # and ends with "USA"
 if x:
-  print("YES! We have a match!")
+    print("YES! We have a match!")
 else:
-  print("No match")
+    print("No match")
+
+txt2 = "hello planet"
+
+# Search for a sequence that starts with "he", followed by 0 or 1  (any) character, and an "o":
+
+y = re.findall("he.?o", txt2)
+
+print(y)
+
+# This time we got no match, because there were not zero, not one, but two characters between "he" and the "o"
